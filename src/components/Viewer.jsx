f92c448111013fe63f00a1423ad56ae11075d0d4
junnifront <jiyoonlee07@naver.com>
2024-12-10 21:46:07 +0900

    1 commit

---
+++ src/components/Viewer.jsx
@@ -0,0 +1,13 @@
+const Viewer =({count}) => {
+    return (
+        <div>
+            <div>
+                Current Count :
+            </div>
+            <h1>{count}</h1>
+
+        </div>
+    )
+}
+
+export default Viewer;
